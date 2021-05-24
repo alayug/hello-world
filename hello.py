bba883201e2588740eb2bb67a2bb00b02a308015
ChHarding <charding@iastate.edu>
2021-05-24 12:10:22 -0500

    Added print()

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
 # This # sign allows me to write comments
 # This line prints 'Hello, world! This is my first python code'
 print('Hello, world! This is my first python code')
+
+print("Well done!")
